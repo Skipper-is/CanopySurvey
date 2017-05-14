881a46950884991d3c498760a724c4457a70b5e5
Skipper-is <skipperthepilot@googlemail.com>
2017-05-14 13:58:06 +0100

    Starting to add location data

--- app/src/main/java/com/skipper/canopysurvey/ProcessPreview.java
+++ app/src/main/java/com/skipper/canopysurvey/ProcessPreview.java
@@ -1,5 +1,6 @@
 package com.skipper.canopysurvey;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -21,12 +22,13 @@ import java.io.ByteArrayOutputStream;
 public class ProcessPreview extends AppCompatActivity {
     private static ImageView imageView;
     private static SeekBar seekBar;
-    private static Button process, reset;
+    private static Button process, reset, next;
     private static TextView progressText, percentageCover;
     private static Bitmap initialImage;
     private static float cover = 0;
     private String byteArrayName;
     private byte[] byteArray;
+    private static int done = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +45,7 @@ public class ProcessPreview extends AppCompatActivity {
         process = (Button) findViewById(R.id.process);
         progressText = (TextView) findViewById(R.id.seekBarProgress);
         percentageCover = (TextView) findViewById(R.id.percentageCover);
+        next = (Button) findViewById(R.id.nextGetLocation);
 
         progressText.setText(String.valueOf(seekBar.getProgress()));
 
@@ -55,18 +58,29 @@ public class ProcessPreview extends AppCompatActivity {
 
             percentageCover.setText(String.format("%.2f",savedInstanceState.getFloat("percentCover")) + "% cover");
             progressText.setText(String.valueOf(savedInstanceState.getInt("seekBarProgress")));
-            percentageCover.setVisibility(View.VISIBLE);
+            if (done == 1){
+                percentageCover.setVisibility(View.VISIBLE);
+                next.setVisibility(View.VISIBLE);
+                 }else{
+                percentageCover.setVisibility(View.INVISIBLE);
+                next.setVisibility(View.INVISIBLE);
+            }
+
+
 
             Bitmap restoredImage = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
             imageView.setImageBitmap(restoredImage);
 
-        }else{
+        }else{ //this is in case we've not already saved/rotated/processed etc the images.
             byteArrayName = "image";
 
             byteArray = getIntent().getExtras().getByteArray(byteArrayName);
 
             initialImage = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
             imageView.setImageBitmap(initialImage);
+
+            percentageCover.setVisibility(View.INVISIBLE);
+            next.setVisibility(View.INVISIBLE);
         }
 
 
@@ -99,7 +113,13 @@ public class ProcessPreview extends AppCompatActivity {
         imageView.setImageBitmap(initialImage);
         percentageCover.setText("");
         percentageCover.setVisibility(View.INVISIBLE);
+        next.setVisibility(View.INVISIBLE);
+        done = 0;
     }
+
+    public void nextPressed (View v){
+
+            }
 
     public static Bitmap runProcessing(Bitmap before){
         if (before !=null){
@@ -131,6 +151,8 @@ public class ProcessPreview extends AppCompatActivity {
             cover = ((float)black/(float)total)*100;
             percentageCover.setText(String.format("%.2f",cover) + "% cover");
             percentageCover.setVisibility(View.VISIBLE);
+            done = 1;
+            next.setVisibility(View.VISIBLE);
             return after;
         } else {
             return null;
