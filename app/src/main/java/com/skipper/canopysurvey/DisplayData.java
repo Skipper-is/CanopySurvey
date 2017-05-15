d3b7a28bbd885f9b2306209e8f3579087366f494
Skipper-is <skipperthepilot@googlemail.com>
2017-05-15 09:50:16 +0100

    Gallery rescale issue addressed

--- app/src/main/java/com/skipper/canopysurvey/DisplayData.java
+++ app/src/main/java/com/skipper/canopysurvey/DisplayData.java
@@ -1,13 +1,18 @@
 package com.skipper.canopysurvey;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -15,6 +20,8 @@ import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import static java.security.AccessController.getContext;
 
 public class DisplayData extends AppCompatActivity {
 
@@ -26,19 +33,26 @@ public class DisplayData extends AppCompatActivity {
         TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayout);
 
         //add header to table
-        TableRow rowHeader = new TableRow(this);
+        TableRow row = new TableRow(this);
 
-        rowHeader.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
+        params.setMargins(dpToInt(2),dpToInt(2),dpToInt(2),dpToInt(2));
+        params.gravity = Gravity.CENTER;
+        row.setLayoutParams(params);
+
         String[] headerText={"ID","Image","Cover %", "Delete",};
         for (String c:headerText){
             TextView tv = new TextView(this);
-            tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-            tv.setGravity(Gravity.CENTER);
-            tv.setTextSize(16);
+
+            tv.setLayoutParams(params);
+
+            tv.setTextSize(dpToInt(5));
+            tv.setTextColor(ContextCompat.getColor(this,R.color.backgroundColour));
             tv.setText(c);
-            rowHeader.addView(tv);
+            row.addView(tv);
         }
-        tableLayout.addView(rowHeader);
+        row.setBackgroundResource(R.color.colorPrimaryDark);
+        tableLayout.addView(row);
 
         final DatabaseHelper db = new DatabaseHelper(this);
         SQLiteDatabase database = db.getReadableDatabase();
@@ -55,42 +69,44 @@ public class DisplayData extends AppCompatActivity {
                     double lat = cursor.getDouble(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME_LAT));
                     double lng = cursor.getDouble(cursor.getColumnIndex(DatabaseHelper.COLUMN_NAME_LON));
 
-                    TableRow row = new TableRow(this);
-                    row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+                    row = new TableRow(this);
+                    row.setLayoutParams(params);
 
                     //Oh this is annoying, I'm going to have to do each one individually
                     TextView tv = new TextView(this);
-                    tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-                    tv.setGravity(Gravity.CENTER);
-                    tv.setTextSize(10);
+
+                    tv.setLayoutParams(params);
+                    tv.setTextSize(dpToInt(5));
                     tv.setText(String.valueOf(_id));
                     row.addView(tv);
 
                     //Image! This is the tricky bit
                     ImageView imageView = new ImageView(this);
-                    imageView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, 400));
+
                     imageView.setImageBitmap(new bitmapHelper().bitmapHelper(byteArray));
+                    imageView.setLayoutParams(params);
                     row.addView(imageView);
 
 
                     tv = new TextView(this);
-                    tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-                    tv.setGravity(Gravity.CENTER);
+                    tv.setLayoutParams(params);
 
-                    tv.setTextSize(10);
+                    tv.setTextSize(dpToInt(5));
                     tv.setText(String.format("%.2f",cover) + "%");
                     row.addView(tv);
 
                     ImageButton imageButton = new ImageButton (this);
                     Drawable icon = getResources().getDrawable(android.R.drawable.ic_delete);
                     imageButton.setBackgroundDrawable(icon);
-                    imageButton.setLayoutParams(new TableRow.LayoutParams(150, 150));
+                    imageButton.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT));
+
 
                     imageButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             deleteRecord(_id);
 
+                            reset();
                         }
                     });
 
@@ -118,10 +134,15 @@ public class DisplayData extends AppCompatActivity {
 
     }
 
+    private int dpToInt(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+
+    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-       Intent intent = new Intent (this, MainActivity.class);
+        Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);
     }
 
